e1da91fb1c468d2b6d83fbe749a109891093a58e
Chris Reeve <paradigmsareconstructed@gmail.com>
2016-12-12 21:09:07 -0800

    Fixed two broken images, moved assets to /dist

--- src/js/main.js
+++ src/js/main.js
@@ -534,7 +534,7 @@ $(window).load(function() {
 				method: 'feed',
 				name: 'Is It Possible to Increase the Rate of Innovation?',
 				link: 'https://www.worldviewer.github.io',
-				picture: 'https://worldviewer.github.io/src/img-mobile/get-big-things-done-7.jpg',
+				picture: 'https://worldviewer.github.io/dist/img-mobile/get-big-things-done-7.jpg',
 				caption: 'worldviewer.github.io',
 
 				// 297 max
@@ -947,7 +947,7 @@ if (!device.mobile() && !device.tablet() && (Cookies.get('display') !== 'mobile'
 	// Dynamically add in the img tag, so that this huge file never downloads for mobile
 
 	var firstDiv = document.querySelector('#impress > div:first-of-type');
-	firstDiv.insertAdjacentHTML('beforeend', '<img class="big-image" src="src/img-desktop/get-big-things-done-1.1.jpg" alt="Get Big Things Done Infographic">');
+	firstDiv.insertAdjacentHTML('beforeend', '<img class="big-image" src="dist/img-desktop/get-big-things-done-1.1.jpg" alt="Get Big Things Done Infographic">');
 
 	loadScript("dist/js/impress.js", myPrettyCode);
 } else {
