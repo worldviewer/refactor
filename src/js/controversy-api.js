dc5413cee9ddf93fbd5bc56fc55d47f5f315eba3
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-01-06 07:42:26 -0800

    Now communicating with v2 API

--- src/js/controversy-api.js
+++ src/js/controversy-api.js
@@ -1,7 +1,6 @@
 export default class controversyAPI {
 	constructor() {
 		this.url = 'https://worldviewer-test.apigee.net/controversies/v1/';
-		// this.url = 'http://localhost:10010/v1/';
 		this.cardId = '76b02dc7-d246-11e6-861b-0ad881f403bf'; // Example graphic mock
 	}
 
