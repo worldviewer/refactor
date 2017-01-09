b2d6efec9a53e29bcac9e79ab99ad88201c4497f
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-01-09 14:22:19 -0800

    npm run build

--- dist/js/app.js
+++ dist/js/app.js
@@ -543,7 +543,7 @@ var controversyAPI = function () {
 	function controversyAPI() {
 		_classCallCheck(this, controversyAPI);
 
-		this.url = 'https://worldviewer-test.apigee.net/controversies-of-science-api/v1/';
+		this.url = 'https://worldviewer-test.apigee.net/controversies-of-science/v1/';
 		this.cardId = '76b02dc7-d246-11e6-861b-0ad881f403bf'; // Example graphic mock
 	}
 
